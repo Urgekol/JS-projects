c73fdd227b9e532a108c7f909544f51ca3b55172
Urgekol <arghadipbiswas0@gmail.com>
2025-02-04 00:16:37 +0530

    Initial commit

---
+++ color_background/index.js
@@ -0,0 +1,15 @@
+document.getElementById("red").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("blue").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("green").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("reset").addEventListener("click", function() {
+    document.body.style.backgroundColor = "white";
+});
